daa51ab3b4a0f6e56520fcd09892bbd3a49cafdb
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 18:17:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function iterativeLog(array){
   array.forEach((elem, idx) => console.log(`${idx}: ${elem}`));
 }
+
+function iterate(callback){
+  var array = [1,2,3];
+  array.forEach(callback);
+  return array;
+}
+
+function doToArray(array, callback){
+  array.forEach(callback);
+}
