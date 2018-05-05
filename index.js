1657971a2ef6ec70dbc5ade84144a854215f9a97
Murat Celik <cashkl@yandex.com>
2018-05-05 21:13:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function iterativeLog(arr) {
+  arr.forEach((element,index,array) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+
+function iterate(callback) {
+  array = [31,123,123,12]
+
+  array.forEach(callback)
+
+
+  return array;
+}
+
+function doToArray(arr,callback) {
+  arr.forEach(callback)
+  return array
+}
