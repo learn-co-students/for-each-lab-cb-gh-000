9ef46bc694392f336f2f454b31029355dcb6394a
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-05 08:16:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach ((element, index, array) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  var heroes = ["mi", "ni", "ci"];
+  heroes.forEach(callback);
+  return heroes;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+  return array;
+}
