68b404eb1c079fb3dd5e1ca692677b286db83ef2
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-07 18:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function iterativeLog(array){
   return array.forEach((element, index) => {
-    console.log(`${index}: ${element}.`)
+    log(element, index)
   })
+}
+
+function log(element, index){
+  console.log(`${index}: ${element}.`)
 }
